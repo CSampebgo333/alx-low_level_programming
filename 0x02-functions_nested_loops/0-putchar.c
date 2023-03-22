e6d36392b2f981be3012d1c27b57d8da0384cde0
CSampebgo333 <clemsosampebgo7@gmail.com>
2023-03-21 19:53:22 -0700

    correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,3 @@
-0x02. C - Functions, nested loops
-
-
-0-putchar.c
-
 #include "main.h"
 /**
  * main - print _putchar
@@ -12,15 +7,14 @@
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
-
