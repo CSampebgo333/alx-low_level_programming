505f5996ca5431ba49765c3c6f308a026608b2c4
CSampebgo333 <clemsosampebgo7@gmail.com>
2023-04-05 21:03:39 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,9 +4,7 @@
  *
  *  _puts_recursion - prints a string, followed by a new line
  *
- *  @s - input
- *
- *  return - Always 0
+ *  @s - The string to be printed
  */
 void _puts_recursion(char *s)
 {	
