495e1cab57da120a87d3e55ba23bc384c8745147
CSampebgo333 <clemsosampebgo7@gmail.com>
2023-04-09 03:06:53 -0700

    task_02

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: The string to be count
+ *
+ * return - string length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return(1 + _strlen_recursion(s + 1);
+	}
+	else
+	{
+		return(0);
+	}
+}
