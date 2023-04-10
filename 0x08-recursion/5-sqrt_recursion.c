97057344ef0b9b99ac66c1494c62a1391b866452
CSampebgo333 <clemsosampebgo7@gmail.com>
2023-04-09 23:21:55 -0700

    Correction

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,31 +5,30 @@ int actual_sqrt_recursion(int n, int i);
 /**
  * _sqrt_recursion -  returns the natural square root of a number
  *
- * @n: number to be squared
+ * @n: number to square of
  *
- * Return: the square of the number
+ * Return: the natural square
  */
 int _sqrt_recursion(int n)
 {
-        if (n < 0)
-        {
-                return (-1);
-        }
-        else
-        {
-                return (actual_sqrt_recursion(n, 0));
-        }
-
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (actual_sqrt_recursion(n, 0));
+	}
 }
 
 /**
- * actual_sqrt_recursion - Calculate actual  square
+ * actual_sqrt_recursion - Calculate the natural square of a number
  *
- * @n: number to be squared
+ * @n: number to square of
  *
  * @i: indicator
  *
- * Return: square root
+ * Return: the square of the number
  */
 int actual_sqrt_recursion(int n, int i)
 {
