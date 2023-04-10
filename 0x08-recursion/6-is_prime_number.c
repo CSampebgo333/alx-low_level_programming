cbe8ae274372f73b49d385b8da05dee90c81d4ca
CSampebgo333 <clemsosampebgo7@gmail.com>
2023-04-10 12:16:17 -0700

    Task_06

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+int actual_prime(int n, int i);
+
+/**
+ * is_prime_number -  returns 1 if the input integer is a prime number, otherwise return 0
+ *
+ * @n: The number to be checked
+ *
+ * Return: 1 or 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (actual_prime(n, n-1));
+	}
+}
+
+/**
+ * actual_prime - return 1 if n is prime number else if return 0
+ *
+ * @n: The number to be checked 
+ *
+ * @i: iterator
+ *
+ * Return: 1 if success or 0 if failed
+ */
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+	{
+		return (0);
+	}
+	if (n % i == 0 && i > 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (actual_prime(n, i-1));
+	}
+}
