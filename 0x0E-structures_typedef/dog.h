6ed038311fbf5bab33fa78329ddae20366465f20
CSampebgo333 <clemsosampebgo7@gmail.com>
2023-04-19 00:07:56 +0100

    Corrections

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
-#ifndef
-#define
+#ifndef DOG
+#define DOG
 
+/**
+ * struct dog - dog class
+ *
+ * @name: dog name
+ *
+ * @age: dog age
+ *
+ * @owner: dog owner
+ */
 struct dog
 {
 	char * name;
