72c6c922806cda70499b60cb3486c6ca1d964ed5
CSampebgo333 <clemsosampebgo7@gmail.com>
2023-04-18 19:00:01 -0700

    Correction again

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,29 +6,21 @@
  *
  * @d: pointer to struct dog
  *
- * Return: structur
+ * Return: structure
  */
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
 		if (d->name != NULL)
-		{
 			printf("Name: %s\n", d->name);
-		}
 		else
-		{
 			printf("Name: (nil)\n");
-		}
 		printf("Age: %.3f\n", d->age);
 
 		if (d->owner != NULL)
-		{
 			printf("Owner: %s\n", d->owner);
-		}
 		else
-		{
 			printf("Owner: (nil)\n");
-		}
 	}
 }
