56d4c309e0a2186caa25dd68b32c2064dc2bec5b
CSampebgo333 <clemsosampebgo7@gmail.com>
2023-05-02 08:20:20 -0700

    Correction

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,8 +3,11 @@
 
 /**
  * _strncpy - copie a string
+ *
  * @dest: the string to be complet
+ *
  * @src: the string to be copied
+ *
  * @n: numbre of character to be appended
  *
  * Return: copied string
@@ -13,7 +16,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0, count = 0;
 
-	while (src[i] && n >= 0)
+	while (src[i] && n > 0)
 	{
 		dest[i] = src[i];
 		i++;
