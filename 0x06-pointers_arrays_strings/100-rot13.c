595ef2bf9fe09f087eb51bd32dec223f98a8745d
CSampebgo333 <clemsosampebgo7@gmail.com>
2023-05-05 11:01:38 -0700

    Task_100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @rot: the string to be encoding
+ *
+ * Return: the encoded string
+ */
+char *rot13(char *rot)
+{
+	int i, j;
+	char *a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *n = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; rot[i]; i++)
+	{
+		for (j = 0; a[j]; j++)
+		{
+			if (rot[i] == a[j])
+			{
+				rot[i] == n[j];
+			}
+		}
+	}
+	return (rot);
+}
