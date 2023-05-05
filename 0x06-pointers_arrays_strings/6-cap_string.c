177dbf7a9724c452ed482234fd0b0584051709ad
CSampebgo333 <clemsosampebgo7@gmail.com>
2023-05-05 04:41:14 -0700

    Last correction

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,34 +8,25 @@
  */
 char *cap_string(char *str)
 {
-	int i = 0, j;
-	char c[] = " \t\n,;.!?\"(){}";
-
-	while (str[i])
-	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-		{
-			if (i == 0)
-			{
-				str[i] = str[i] - 32;
-			}
-			else
-			{
-				for (j = 0; j < 13; j++)
-				{
-					if (str[i - 1] == c[j])
-					{
-						str[i] = str[i] - 32;
-					}
-				}
-			}
-		}
-
-		if (str[i] == '\t')
-		{
-			str[i] = ' ';
-		}
-		i++;
-	}
-	return (str);
+        int i = 0, j;
+        char a[] = " \t\n,;.!?\"(){}";
+	
+        while (*(str + i))
+        {
+                if (*(str + i) >= 'a' && *(str + i) <= 'z')
+                {
+                        if (i == 0)
+                                *(str + i) -= 'a' - 'A';
+                        else
+                        {
+                                for (j = 0; j <= 12; j++)
+                                {
+                                        if (a[j] == *(str + i - 1))
+                                                *(str + i) -= 'a' - 'A';
+                                }
+                        }
+                }
+                i++;
+        }
+        return (str);
 }
