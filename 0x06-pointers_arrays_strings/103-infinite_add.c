0aca9cf6a4cb6d1f98cac81ff68db4085c736937
CSampebgo333 <clemsosampebgo7@gmail.com>
2023-05-07 13:15:43 -0700

    Correction of magic file

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * infinite_add - adds two numbers long numbers.
+ * @n1: first number
+ * @n2: second number
+ * @r: the buffer that the function will use to store the result
+ * @size_r: the buffer size
+ *
+ * Return: returns a pointer to the result
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i = 0, j = 0;
